6d3d4a5b4d836a4c1bb0217eaa54066a2bd42b89
yash797 <80116786+yash797@users.noreply.github.com>
2022-11-23 11:48:27 +0530

    Update 6.m

--- 6.m
+++ 6.m
@@ -71,6 +71,79 @@ scatterplot(r); % Constellation Diagram with Noise
 %Objective-2:
 %16-Square QAM in the presence of AWGN for ML receiver.
 
+%Write a program to plot signal constellation diagram of received
+% 16-Square QAM signal in the presence of AWGN.
+
+clc;
+clear all;
+close all;
+pkg load communications
+N = 16000; % Number of bits to be transmitted using 16-Square QAM
+ % Too large value may slow down the program
+x = randi([0,1],1,N); % Random input bits generation
+M = 16; % Number of Symbols in 16-Square QAM
+d = sqrt(2/5); % Average symbol energy is normalized to unity
+% Symbol Generation
+yy = [];
+for i=1:4:length(x)
+ if x(i)==0 && x(i+1)==0 & x(i+2)==0 & x(i+3)==0
+ y = -3*d/2+j*(-3*d/2);
+ elseif x(i)==0 && x(i+1)==0 & x(i+2)==0 & x(i+3)==1
+ y = -3*d/2+j*(-d/2);
+ elseif x(i)==0 && x(i+1)==0 & x(i+2)==1 & x(i+3)==1
+ y = -3*d/2+j*(d/2);
+ elseif x(i)==0 && x(i+1)==0 & x(i+2)==1 & x(i+3)==0
+ y = -3*d/2+j*(3*d/2);
+ elseif x(i)==0 && x(i+1)==1 & x(i+2)==0 & x(i+3)==0
+ y = -d/2+j*(-3*d/2);
+ elseif x(i)==0 && x(i+1)==1 & x(i+2)==0 & x(i+3)==1
+ y = -d/2+j*(-d/2);
+ elseif x(i)==0 && x(i+1)==1 & x(i+2)==1 & x(i+3)==1
+ y = -d/2+j*(d/2);
+ elseif x(i)==0 && x(i+1)==1 & x(i+2)==1 & x(i+3)==0
+ y = -d/2+j*(3*d/2);
+ elseif x(i)==1 && x(i+1)==1 & x(i+2)==0 & x(i+3)==0
+ y = d/2+j*(-3*d/2);
+ elseif x(i)==1 && x(i+1)==1 & x(i+2)==0 & x(i+3)==1
+ y = d/2+j*(-d/2);
+ elseif x(i)==1 && x(i+1)==1 & x(i+2)==1 & x(i+3)==1
+ y = d/2+j*(d/2);
+ elseif x(i)==1 && x(i+1)==1 & x(i+2)==1 & x(i+3)==0
+ y = d/2+j*(3*d/2);
+ elseif x(i)==1 && x(i+1)==0 & x(i+2)==0 & x(i+3)==0
+ y = 3*d/2+j*(-3*d/2);
+ elseif x(i)==1 && x(i+1)==0 & x(i+2)==0 & x(i+3)==1
+ y = 3*d/2+j*(-d/2);
+ elseif x(i)==1 && x(i+1)==0 & x(i+2)==1 & x(i+3)==1
+ y = 3*d/2+j*(d/2);
+ elseif x(i)==1 && x(i+1)==0 & x(i+2)==1 & x(i+3)==0
+ y = 3*d/2+j*(3*d/2);
+ endif
+% Transmitted Symbols
+yy = [yy y];
+endfor
+scatterplot(yy); % Constellation Diagram without Noise
+EbN0db = 20; % Change this value & run program to see the noisy constellation
+EbN0 = 10^(EbN0db/10);
+% AWGN Channel
+n = (1/sqrt(2))*[randn(1,length(yy)) + 1j*randn(1,length(yy))];
+sigma = sqrt(1/((log2(M))*EbN0));
+% Received Symbols
+r = yy + sigma*n;
+scatterplot(r); % Constellation Diagram with Noise
+
+
+
+
+
+
+
+
+
+
+%Objective-2:
+%16-Square QAM in the presence of AWGN for ML receiver.
+
 clc;
 clear all;
 close all;
@@ -122,9 +195,7 @@ endfor
 % Detection based on euclidean distance
 ber_simulated = [];
 ber_theoretical = [];
-ref_symbols = [-3*d/2+j*(-3*d/2) -3*d/2+j*(-d/2) -3*d/2+j*d/2 -3*d/2+j*(3*d/2) -d/2+j*(-3*d/2) -
-d/2+j*(-d/2) -d/2+j*d/2 -d/2+j*(3*d/2) d/2+j*(-3*d/2) d/2+j*(-d/2) d/2+j*d/2 d/2+j*(3*d/2)
-3*d/2+j*(-3*d/2) 3*d/2+j*(-d/2) 3*d/2+j*d/2 3*d/2+j*(3*d/2)];
+ref_symbols = [-3*d/2+j*(-3*d/2) -3*d/2+j*(-d/2) -3*d/2+j*d/2 -3*d/2+j*(3*d/2) -d/2+j*(-3*d/2) - d/2+j*(-d/2) -d/2+j*d/2 -d/2+j*(3*d/2) d/2+j*(-3*d/2) d/2+j*(-d/2) d/2+j*d/2 d/2+j*(3*d/2) -3*d/2+j*(-3*d/2) 3*d/2+j*(-d/2) 3*d/2+j*d/2 3*d/2+j*(3*d/2)];
 for EbN0db = 0:15
 EbN0 = 10^(EbN0db/10);
 n = (1/sqrt(2))*[randn(1,length(yy)) + 1j*randn(1,length(yy))];
